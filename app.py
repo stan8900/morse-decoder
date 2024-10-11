a92ffe19da41b32d6f49833971562391101e5f90
MukhammadSultan-Jurabekov <jurabekovmukhammadsultan@gmail.com>
2024-10-11 19:10:36 +0100

    Function to decode Morse code

--- app.py
+++ app.py
@@ -11,3 +11,15 @@ MORSE_CODE_DICT = {
     '6': '-....', '7': '--...', '8': '---..', '9': '----.', '0': '-----', ' ': '/'
 }
 
+# Function to decode Morse code
+def morse_to_text(morse_code):
+    words = morse_code.split(' / ')  # Words separated by '/'
+    decoded_message = []
+    
+    for word in words:
+        letters = word.split(' ')
+        decoded_word = ''.join([key for letter in letters for key, value in MORSE_CODE_DICT.items() if value == letter])
+        decoded_message.append(decoded_word)
+    
+    return ' '.join(decoded_message)
+
